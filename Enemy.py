14c1b1fb7add0ae10c2da062a599cc00b26ae1c1
Pat Myron <pmyron09@gmail.com>
2016-05-02 19:19:11 -0400

    luigi joining in

--- Enemy.py
+++ Enemy.py
@@ -3,12 +3,14 @@ class Enemy(pygame.sprite.Sprite):
         def __init__(self, gs=None):
                 pygame.sprite.Sprite.__init__(self)
                 self.gs = gs
-                self.image = pygame.image.load("media/globe.png")
+		self.sprite = 0
+                self.image = pygame.image.load("media/luigi/luigi-" + str(self.sprite + 1) +  ".png")
                 self.rect = self.image.get_rect()
 		self.rect.x = 450
-		self.rect.y = 330
+		self.rect.y = 364
 
                 # keep original image to limit resize errors
                 self.orig_image = self.image
         def tick(self):
-		pass
+		self.sprite = (self.sprite + 1) % 3  # incrementing sprite
+                self.image = pygame.image.load("media/luigi/luigi-" + str(self.sprite + 1) +  ".png")
