0dc017a22522b42a4238328646f95ca9d3631975
Pat Myron <pmyron09@gmail.com>
2017-02-11 00:50:38 -0500

    converting some boundaries to a saner format

--- two-player/bounds.py
+++ two-player/bounds.py
@@ -10,189 +10,130 @@ def check(x, y):
 	if (top > 365):
 		if ((left > 2200 and right < 2263) or (left > 2746 and right < 2843) or	(left >	4888 and right < 4950)): # gaps
 			pass
-                else: # ground
-                        return False
+		else: # ground
+			return False
 
 
+	arrays = [
 	# pipes
+	[890, 333, 952, 397],
+	[1209, 301, 1273, 397],
+	[1464, 270, 1528, 396],
+	[1816, 270, 1878, 398],
+	[5208, 333, 5269, 397],
+	[5719, 333, 5782, 397],
+	#blocks
+	[505, 270, 537, 300],
+	[633, 270, 793, 300],
+	[697, 140, 729, 173],
+	[2456, 270, 2552, 300],
+	[2551, 140, 2807, 173],
+	[2905, 140, 3032, 173],
+	[3000, 270, 3032, 300],
+	[3191, 270, 3255, 300],
+	[3384, 270, 3415, 300],
+	[3480, 270, 3511, 300],
+	[3576, 270, 3607, 300],
+	[3480, 140, 3511, 173],
+	[3768, 270, 3801, 300],
+	[3864, 140, 3960, 173],
+	[4086, 140, 4215, 173],
+	[4119, 270, 4183, 300],
+	[5368, 270, 5496, 300],
+	[6329, 364, 6362, 397]
+	]
+	for array in arrays:
+		if (right > array[0]) and (left < array[2]):
+			if (bottom > array[1]) and (top < array[3]):
+				return False
+	
 
-	if (right > 890) and (left < 952):
-		if (bottom > 333) and (top < 397):
-			return False
-
-        if (right > 1209) and (left < 1273):
-                if (bottom > 301) and (top < 397):
-                        return False
-        if (right > 1464) and (left < 1528):
-                if (bottom > 270) and (top < 397):
-			return False
-        if (right > 1816) and (left < 1878):
-                if (bottom > 270) and (top < 397):
-			return False
-        if (right > 5208) and (left < 5269):
-                if (bottom > 333) and (top < 397):
-			return False
-        if (right > 5719) and (left < 5782):
-                if (bottom > 333) and (top < 397):
-                        return False
-
-
-	# blocks
-
-        if (right > 505) and (left < 537):
-                if (bottom > 270) and (top < 300):
-                        return False
-
-        if (right > 633) and (left < 793):
-                if (bottom > 270) and (top < 300):
-                        return False
-        if (right > 697) and (left < 729):
-                if (bottom > 140) and (top < 173):
-                        return False
-        if (right > 2456) and (left < 2552):
-                if (bottom > 270) and (top < 300):
-                        return False
-        if (right > 2551) and (left < 2807):
-                if (bottom > 140) and (top < 173):
-                        return False
-        if (right > 2905) and (left < 3032):
-                if (bottom > 140) and (top < 173):
-                        return False
-        if (right > 3000) and (left < 3032):
-                if (bottom > 270) and (top < 300):
-                        return False
-                        
-        if (right > 3191) and (left < 3255):
-                if (bottom > 270) and (top < 300):
-                        return False
-                        
-        if (right > 3384) and (left < 3415):
-                if (bottom > 270) and (top < 300):
-                        return False
- 
-        if (right > 3480) and (left < 3511):
-                if (bottom > 270) and (top < 300):
-                        return False
- 
-        if (right > 3576) and (left < 3607):
-                if (bottom > 270) and (top < 300):
-                        return False
- 
-        if (right > 3480) and (left < 3511):
-                if (bottom > 140) and (top < 173):
-                        return False
- 
-        if (right > 3768) and (left < 3801):
-                if (bottom > 270) and (top < 300):
-                        return False
- 
-        if (right > 3864) and (left < 3960):
-                if (bottom > 140) and (top < 173):
-                        return False
- 
-        if (right > 4086) and (left < 4215):
-                if (bottom > 140) and (top < 173):
-                        return False
- 
-        if (right > 4119) and (left < 4183):
-                if (bottom > 270) and (top < 300):
-                        return False
- 
-        if (right > 5368) and (left < 5496):
-                if (bottom > 270) and (top < 300):
-                        return False
-                        
-        if (right > 6329) and (left < 6362):
-                if (bottom > 364) and (top < 397):
-                        return False
-                        
-                        
 	# stairs
 	
 	# 1st staircase
-        if (right > 4280) and (left < 4407):
-                if (bottom > 365) and (top < 397):
-                        return False
-        if (right > 4312) and (left < 4407):
-                if (bottom > 333) and (top < 397):
-                        return False
-        if (right > 4344) and (left < 4407):
-                if (bottom > 300) and (top < 397):
-                        return False
-        if (right > 4376) and (left < 4407):
-                if (bottom > 269) and (top < 397):
-                        return False
+	if (right > 4280) and (left < 4407):
+			if (bottom > 365) and (top < 397):
+					return False
+	if (right > 4312) and (left < 4407):
+			if (bottom > 333) and (top < 397):
+					return False
+	if (right > 4344) and (left < 4407):
+			if (bottom > 300) and (top < 397):
+					return False
+	if (right > 4376) and (left < 4407):
+			if (bottom > 269) and (top < 397):
+					return False
 	
 
 	# 3rd staircase
-        if (right > 4727) and (left < 4888):
-                if (bottom > 365) and (top < 397):
-                        return False
-        if (right > 4759) and (left < 4888):
-                if (bottom > 333) and (top < 397):
-                        return False
-        if (right > 4791) and (left < 4888):
-                if (bottom > 300) and (top < 397):
-                        return False
-        if (right > 4823) and (left < 4888):
-                if (bottom > 269) and (top < 397):
-                        return False
+	if (right > 4727) and (left < 4888):
+			if (bottom > 365) and (top < 397):
+					return False
+	if (right > 4759) and (left < 4888):
+			if (bottom > 333) and (top < 397):
+					return False
+	if (right > 4791) and (left < 4888):
+			if (bottom > 300) and (top < 397):
+					return False
+	if (right > 4823) and (left < 4888):
+			if (bottom > 269) and (top < 397):
+					return False
 	
 	
 	# 5th staircase
-        if (right > 5784) and (left < 6073):
-                if (bottom > 365) and (top < 397):
-                        return False
-        if (right > 5816) and (left < 6073):
-                if (bottom > 333) and (top < 397):
-                        return False
-        if (right > 5848) and (left < 6073):
-                if (bottom > 300) and (top < 397):
-                        return False
-        if (right > 5880) and (left < 6073):
-                if (bottom > 269) and (top < 397):
-                        return False
-        if (right > 5912) and (left < 6073):
-                if (bottom > 237) and (top < 397):
-                        return False
-        if (right > 5944) and (left < 6073):
-                if (bottom > 205) and (top < 397):
-                        return False
-        if (right > 5976) and (left < 6073):
-                if (bottom > 173) and (top < 397):
-                        return False
-        if (right > 6008) and (left < 6073):
-                if (bottom > 140) and (top < 397):
-                        return False
+	if (right > 5784) and (left < 6073):
+			if (bottom > 365) and (top < 397):
+					return False
+	if (right > 5816) and (left < 6073):
+			if (bottom > 333) and (top < 397):
+					return False
+	if (right > 5848) and (left < 6073):
+			if (bottom > 300) and (top < 397):
+					return False
+	if (right > 5880) and (left < 6073):
+			if (bottom > 269) and (top < 397):
+					return False
+	if (right > 5912) and (left < 6073):
+			if (bottom > 237) and (top < 397):
+					return False
+	if (right > 5944) and (left < 6073):
+			if (bottom > 205) and (top < 397):
+					return False
+	if (right > 5976) and (left < 6073):
+			if (bottom > 173) and (top < 397):
+					return False
+	if (right > 6008) and (left < 6073):
+			if (bottom > 140) and (top < 397):
+					return False
 	
 	
 	# 2nd staircase
-        if (right > 4470) and (left < 4599):
-                if (bottom > 365) and (top < 397):
-                        return False
-        if (right > 4470) and (left < 4567):
-                if (bottom > 333) and (top < 397):
-                        return False
-        if (right > 4470) and (left < 4535):
-                if (bottom > 300) and (top < 397):
-                        return False
-        if (right > 4470) and (left < 4503):
-                if (bottom > 269) and (top < 397):
-                        return False
+	if (right > 4470) and (left < 4599):
+			if (bottom > 365) and (top < 397):
+					return False
+	if (right > 4470) and (left < 4567):
+			if (bottom > 333) and (top < 397):
+					return False
+	if (right > 4470) and (left < 4535):
+			if (bottom > 300) and (top < 397):
+					return False
+	if (right > 4470) and (left < 4503):
+			if (bottom > 269) and (top < 397):
+					return False
 
-                        
+						
 	# 4th staircase
-        if (right > 4950) and (left < 5080):
-                if (bottom > 365) and (top < 397):
-                        return False
-        if (right > 4950) and (left < 5048):
-                if (bottom > 333) and (top < 397):
-                        return False
-        if (right > 4950) and (left < 5016):
-                if (bottom > 300) and (top < 397):
-                        return False
-        if (right > 4950) and (left < 4984):
-                if (bottom > 269) and (top < 397):
-                        return False
+	if (right > 4950) and (left < 5080):
+			if (bottom > 365) and (top < 397):
+					return False
+	if (right > 4950) and (left < 5048):
+			if (bottom > 333) and (top < 397):
+					return False
+	if (right > 4950) and (left < 5016):
+			if (bottom > 300) and (top < 397):
+					return False
+	if (right > 4950) and (left < 4984):
+			if (bottom > 269) and (top < 397):
+					return False
 	
 	return True
