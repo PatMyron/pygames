4b9969d25bb21e1af8e15298a6804bff50bce915
Pat Myron <pmyron09@gmail.com>
2017-03-07 03:29:31 -0500

    cleanup

--- one-player/GameSpace.py
+++ one-player/GameSpace.py
@@ -8,35 +8,36 @@ from Player import Player
 
 
 class GameSpace:
-        def __init__(self):
-		pass
-        def main(self):
-                pygame.init()
-                self.size = self.width, self.height = 900, 444
-                self.black = 0, 0, 0
-                self.screen = pygame.display.set_mode(self.size)
+    def __init__(self):
+        pass
 
-		self.player = Player(self)
-		self.enemy = Enemy(self)		
-		self.bg = Background(self, self.player)
-		
-		go = LoopingCall(self.ticker)
-		go.start(0.01)
-		reactor.run()
+    def main(self):
+        pygame.init()
+        self.size = self.width, self.height = 900, 444
+        self.black = 0, 0, 0
+        self.screen = pygame.display.set_mode(self.size)
 
-	def ticker(self):
-		pygame.event.get()
-		self.player.tick()
-		self.enemy.tick()
-		self.bg.tick()		
+        self.player = Player(self)
+        self.enemy = Enemy(self)
+        self.bg = Background(self, self.player)
 
-		self.screen.fill(self.black)
-		self.screen.blit(self.bg.image, self.bg.rect)
-		self.screen.blit(self.player.image, self.player.rect)
-#		self.screen.blit(self.enemy.image, self.enemy.rect)
-		pygame.display.flip()
+        go = LoopingCall(self.ticker)
+        go.start(0.01)
+        reactor.run()
+
+    def ticker(self):
+        pygame.event.get()
+        self.player.tick()
+        self.enemy.tick()
+        self.bg.tick()
+
+        self.screen.fill(self.black)
+        self.screen.blit(self.bg.image, self.bg.rect)
+        self.screen.blit(self.player.image, self.player.rect)
+        # self.screen.blit(self.enemy.image, self.enemy.rect)
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
-        gs = GameSpace()
-        gs.main()
+    gs = GameSpace()
+    gs.main()
