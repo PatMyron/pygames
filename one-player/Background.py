d4d1ead36919da94705cbc938b87b468c63f5326
Pat Myron <pmyron09@gmail.com>
2017-03-07 16:12:52 -0500

    cleanup

--- one-player/Background.py
+++ one-player/Background.py
@@ -12,6 +12,4 @@ class Background(pygame.sprite.Sprite):
         self.rect.y = 0
 
     def tick(self):
-        self.rect.x = -1 * self.player.x + 450
-
-# 396-397 px down to ground
+        self.rect.x = 450 - self.player.x
