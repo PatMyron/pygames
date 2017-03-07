ee236ffee13d3ae3d4bbe0ad0d02e564096743a2
Pat Myron <pmyron09@gmail.com>
2017-03-07 03:14:18 -0500

    cleanup

--- one-player/Enemy.py
+++ one-player/Enemy.py
@@ -1,16 +1,19 @@
 import pygame
+
+
 class Enemy(pygame.sprite.Sprite):
         def __init__(self, gs=None):
                 pygame.sprite.Sprite.__init__(self)
                 self.gs = gs
-		self.sprite = 0
-                self.image = pygame.image.load("media/luigi/luigi-" + str(self.sprite + 1) +  ".png")
+                self.sprite = 0
+                self.image = pygame.image.load("media/luigi/luigi-" + str(self.sprite + 1) + ".png")
                 self.rect = self.image.get_rect()
-		self.rect.x = 450
-		self.rect.y = 364
+                self.rect.x = 450
+                self.rect.y = 364
 
                 # keep original image to limit resize errors
                 self.orig_image = self.image
+
         def tick(self):
-		self.sprite = (self.sprite + 1) % 3  # incrementing sprite
-                self.image = pygame.image.load("media/luigi/luigi-" + str(self.sprite + 1) +  ".png")
+                self.sprite = (self.sprite + 1) % 3  # incrementing sprite
+                self.image = pygame.image.load("media/luigi/luigi-" + str(self.sprite + 1) + ".png")
--- two-player/Enemy.py
+++ two-player/Enemy.py
@@ -1,4 +1,6 @@
 import pygame
+
+
 class Enemy(pygame.sprite.Sprite):
         def __init__(self, gs, character):
 		if character == "mario":
