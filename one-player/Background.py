ff0509f58711d4d17111afa63df3fa12a08709e7
Pat Myron <pmyron09@gmail.com>
2017-03-07 03:09:08 -0500

    cleanup

--- one-player/Background.py
+++ one-player/Background.py
@@ -1,9 +1,11 @@
 import pygame
+
+
 class Background(pygame.sprite.Sprite):
         def __init__(self, gs, player):
                 pygame.sprite.Sprite.__init__(self)
                 self.gs = gs
-		self.player = player
+                self.player = player
                 self.image = pygame.image.load("media/level1zoomed.png")
                 self.rect = self.image.get_rect()
                 self.rect.x = 0
@@ -11,8 +13,9 @@ class Background(pygame.sprite.Sprite):
 
                 # keep original image to limit resize errors
                 self.orig_image = self.image
+
         def tick(self):
-		self.rect.x = -1 * self.player.x + 450
+                self.rect.x = -1 * self.player.x + 450
 
 
 # 396-397 px down to ground
