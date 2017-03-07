4c2a4b428668d566e0c333e9559c51f5d81c376d
Pat Myron <pmyron09@gmail.com>
2017-03-07 16:10:58 -0500

    cleanup

--- one-player/Background.py
+++ one-player/Background.py
@@ -11,9 +11,6 @@ class Background(pygame.sprite.Sprite):
         self.rect.x = 0
         self.rect.y = 0
 
-        # keep original image to limit resize errors
-        self.orig_image = self.image
-
     def tick(self):
         self.rect.x = -1 * self.player.x + 450
 
